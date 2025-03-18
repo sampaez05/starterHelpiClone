068092cb2fc83b5359cd26b65dc59b15fd7be467
Samantha Paez <sampaez@udel.edu>
2025-03-18 12:33:36 -0400

    added my name to a team member section in app.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -31,6 +31,7 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
+        <p>Team Members: Samantha Paez,</p>
         <a
           className="App-link"
           href="https://reactjs.org"
