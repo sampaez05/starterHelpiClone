652bb7b93d2306a89b3686d09d0b3af9301ed9b8
nolanKeefe <npkeefe@udel.edu>
2025-04-01 23:09:43 -0400

    Basic description

--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -1,13 +1,19 @@
 
 import { Link } from 'react-router-dom';
 import './App.css';
-import { Button } from 'react-bootstrap';
+import { Button, Container } from 'react-bootstrap';
 
 export const BasicQuestions = () => {
     return (
         <div>
-            <header>Basic Questions</header>
-            <Link to="/"><Button className="Buttons">Home</Button></Link>
+            <header>Basic Questions
+            <Link to="/"><Button className="Buttons">Home</Button></Link></header>
+            <Container>
+                Basic Career assesment
+            </Container>
+            <p>The basic Career assesment asks a few simple questions, please select the answer you most feel fits.
+                At the end of the assesment we will give you a general idea of possible jobs
+            </p>
         </div>
     );
 }
