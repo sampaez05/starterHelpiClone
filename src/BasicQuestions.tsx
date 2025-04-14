00de907299fa05c4de1c106b4f6267deea370f01
jaxsing <jaxsing@udel.edu>
2025-04-14 10:25:16 -0400

    random commit

--- src/BasicQuestions.tsx
+++ src/BasicQuestions.tsx
@@ -1,9 +1,14 @@
 
 import { Link } from 'react-router-dom';
 import './App.css';
-import { Button, Container } from 'react-bootstrap';
+import { Form, Button, Container } from 'react-bootstrap';
 
 export const BasicQuestions = () => {
+    const questions = [
+        "What is your name?",
+        "What is your favorite color?",
+        "What is the airspeed velocity of an unladen swallow?",
+      ];
     return (
         <div>
             <header>Basic Questions
@@ -14,6 +19,18 @@ export const BasicQuestions = () => {
             <p>The basic Career assesment asks a few simple questions, please select the answer you most feel fits.
                 At the end of the assesment we will give you a general idea of possible jobs
             </p>
+            <Form>
+      {questions.map((question, index) => (
+        <Form.Group key={index} className="mb-3">
+          <Form.Label>{question}</Form.Label>
+          <Form.Control
+            type="text"
+            placeholder="Your answer"
+            name={`question-${index}`}
+          />
+        </Form.Group>
+      ))}
+    </Form>
         </div>
     );
 }
