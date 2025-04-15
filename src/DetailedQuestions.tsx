0fb00317f4a88d969ff3f9938ec644da16ba56c9
nolanKeefe <npkeefe@udel.edu>
2025-04-15 14:06:17 -0400

    put the stuff in detailed questions

--- src/DetailedQuestions.tsx
+++ src/DetailedQuestions.tsx
@@ -1,19 +1,62 @@
 import { Link } from 'react-router-dom';
 import './App.css';
-import { Container} from 'react-bootstrap';
+import { Button, Container} from 'react-bootstrap';
 import { MultipleChoiceQuestion } from './QuestionFormat';
+import { useState } from 'react';
 
 export const DetailedQuestions = () => {
+    const questions = [
+
+        {
+            question: "what number is so dang cool",
+            options: ["1", "2", "3"]
+        },
+        {
+            question: "what number is so dang EEVIL",
+            options: ["6", "5", "4"]
+        }
+    ];
+
+    // Set up state for answers and responses dynamically
+    const [questionData, setQuestionData] = useState(
+        questions.map(() => ({
+            answer: '',
+            responded: false
+        }))
+    );
+    const updateAnswer = (index: number, answer: string) => {
+        const updated = [...questionData];
+        updated[index] = {
+            answer,
+            responded: true
+        };
+        setQuestionData(updated);
+    };
     return (
         <div>
-            <header>Detailed Questions</header>
+            <header>
+                Detailed Questions
+                <Link to="/"><Button className="Buttons">Home</Button></Link>
+            </header>
             <Container style={{
                  border: '2px solid black',
                  padding: '10px',
                  borderRadius: '5px',
                  width: '200px',
-                textAlign: 'center'}}>
-                Detailed Career Assesment
+                textAlign: 'center'
+            }}>
+                Basic Career assesment
+              
+                {questions.map((q, index) => (
+                    <MultipleChoiceQuestion
+                        key={index}
+                        question={q.question}
+                        options={q.options}
+                        selected={questionData[index].answer}
+                        setSelected={(a) => updateAnswer(index, a)}
+                    />
+                ))}
+
             </Container>
             <p>The Detailed asic Career assesment asks a few simple questions, please select the answer you most feel fits.
                 At the end of the assesment we will give you a general idea of possible jobs
